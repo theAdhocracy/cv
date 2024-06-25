2706158650a47cf40b9779844fa22f64b0466906
theAdhocracy <murrayadcock@gmail.com>
2024-06-25 11:48:44 +0100

    :sparkles: Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const pluginWebC = require("@11ty/eleventy-plugin-webc");
+
+module.exports = function (eleventyConfig) {
+	// Plugins
+	eleventyConfig.addPlugin(pluginWebC, {
+		components: "src/_includes/components/**/*.webc",
+	});
+	eleventyConfig.addPlugin(EleventyRenderPlugin);
+
+	// Assets
+	eleventyConfig.addPassthroughCopy({ "src/styles": "/styles" });
+	eleventyConfig.addPassthroughCopy({ "src/_includes/assets": "/assets" });
+
+	return {
+		dir: {
+			input: "src",
+		},
+	};
+};
