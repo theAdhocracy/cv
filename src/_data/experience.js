da4a28171ca57ae264a24fe0131d795f5f3051eb
theAdhocracy <murrayadcock@gmail.com>
2024-07-01 18:42:52 +0100

    :memo: CV content

---
+++ src/_data/experience.js
@@ -0,0 +1,108 @@
+function setExperience() {
+	return [
+		{
+			title: "Senior Front-End Engineer",
+			category: "Front End",
+			company: {
+				name: "Appnovation",
+				url: "https://www.appnovation.com/",
+			},
+			location: "Remote (London)",
+			period: {
+				start: "2022",
+				end: "2024",
+			},
+			blurb:
+				"<p>Worked as a Senior Consultant within the Creative Technology team, focusing on the NA and EMEA regions.</p><p>Specialised in the Jamstack (Next.js/Storybook/Astro/SvelteKit/Vercel) and accessibility, advising clients on content modelling (Contentful/Sanity.io) and front-end best practices across a range of projects, from enterprise-scale rebuilds, to greenfield design systems, to a bespoke AI/LLM-backed GUI (ChatGPT/Clause 3/Python/React) for medical data analysis.</p><p>Also developed internal accessibility documentation, including maintaining up-to-date legal guidance for the EU, Canada, UK, and US, whilst helping upskill teams across the business via remote learning sessions and presentations.</p>",
+		},
+		{
+			title: "Front-End Engineer (Mid)",
+			category: "Front End",
+			company: {
+				name: "STech",
+				url: "https://www.stech.co.uk/",
+			},
+			location: "Remote (London)",
+			period: {
+				start: "2020",
+				end: "2022",
+			},
+			blurb:
+				"<p>Developed several new features for the existing Next.js codebase, including custom analytics, animated landing pages (GSAP/Lottie), and dark mode/theme support. Performed accessibility and SEO audits and improvements.</p><p>Significantly increased test coverage (RTL) and prototyped a Cypress E2E test architecture. Led and delivered a greenfield design system project (React/Storybook), used in the redesign of spacecasino.co.uk (Next.js/Apollo/Vercel).</p>",
+		},
+		{
+			title: "Front-End Engineer",
+			category: "Front End",
+			company: {
+				name: "Talent Point",
+				url: "https://www.talentpoint.co/",
+			},
+			location: "London",
+			period: {
+				start: "2018",
+				end: "2020",
+			},
+			blurb:
+				"<p>Built growable.io and relaunched talentpoint.co with a modern tech stack (React/Gatsby/Netlify); personal focus was UI components, accessibility, and UX.</p><p>Set up a headless instance of Craft CMS (GraphQL/S3) and migrated all content.</p>",
+		},
+		{
+			title: "Content Manager",
+			category: "Content",
+			company: {
+				name: "Talent Point",
+				url: "https://www.talentpoint.co/",
+			},
+			location: "London",
+			period: {
+				start: "2018",
+				end: "2020",
+			},
+			blurb:
+				"<p>Line managed the Content Team. Worked with senior stakeholders to develop content and SEO strategy.</p><p>Oversaw the delivery of all social media and web content, including email campaigns (SendGrid).</p>",
+		},
+		{
+			title: "Software Developer",
+			category: "Software",
+			company: {
+				name: "Synertec Ltd.",
+				url: "https://synertec.co.uk/",
+			},
+			location: "Taunton",
+			period: {
+				start: "2016",
+				end: "2018",
+			},
+			blurb:
+				"<p>Part of a small team building greenfield projects, including a new software platform to improve NHS document accessibility.</p><p>Wrote all technical documentation and provided internal training. Reduced implementation time by 85% with a custom automation tool (VBA).</p>",
+		},
+		{
+			title: "Technical Support",
+			category: "QA",
+			company: {
+				name: "Synertec Ltd.",
+				url: "https://synertec.co.uk/",
+			},
+			location: "Taunton",
+			period: {
+				start: "2015",
+				end: "2016",
+			},
+			blurb:
+				"<p>Second-line support using rapid, iterative testing. Developed several automation scripts to replace repetitive internal procedures.</p>",
+		},
+	];
+}
+
+module.exports = setExperience;
+
+// {
+//     title: "",
+//     category: "",
+//     company: "",
+//     location: "",
+//     period: {
+//          start: "",
+//          end: "",
+//     },
+//     blurb: ""
+// },
